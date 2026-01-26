4195fa96228b8a4361e4276753a3473d2dd6c2a4
Pranavi <pranavigaddam12@gmail.com>
2026-01-27 04:00:02 +0530

    chore: bootstrap Spring Boot backend (Java 21)

---
+++ src/main/java/com/express/expressbackend/ExpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.express.expressbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressBackendApplication.class, args);
+	}
+
+}
