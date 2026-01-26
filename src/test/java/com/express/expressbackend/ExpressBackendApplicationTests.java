d2ebc5eedaa8e0d9edd8e7b55ee7ce8628c501a9
Lokeshwar <lokeshwarmarri99@gmail.com>
2026-01-27 04:00:02 +0530

    chore: bootstrap Spring Boot backend (Java 21)

---
+++ src/test/java/com/express/expressbackend/ExpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.expressbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
